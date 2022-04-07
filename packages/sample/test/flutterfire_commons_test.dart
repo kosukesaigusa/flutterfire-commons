d57207b7d1acdb6c34370e8088be0decc0b2a5c9
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-04-07 22:02:31 +0900

    feat: add sample/example app

--- packages/sample/test/flutterfire_commons_test.dart
+++ packages/sample/test/flutterfire_commons_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutterfire_commons/flutterfire_commons.dart';
+import 'package:sample/sample.dart';
 
 void main() {
   test('adds one to input values', () {
