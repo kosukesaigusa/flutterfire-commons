67d5b786a066450dd61ebe8ac4f32db3f62e8c5f
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-04-15 22:29:12 +0900

    fix: fix create_packate_template.sh

--- scripts/create_package_template.sh
+++ scripts/create_package_template.sh
@@ -8,6 +8,6 @@ sed -i '' -e "s/sample/$str/g" ./packages/$str/test/sample_test.dart
 sed -i '' -e "s/sample/$str/g" ./packages/$str/example/pubspec.yaml
 sed -i '' -e "s/sample/$str/g" ./packages/$str/example/lib/main.dart
 mv ./packages/$str/lib/sample.dart ./packages/$str/lib/$str.dart
-mv ./packages/$str/test/sample_test.dart ./packages/$str/test/$str_test.dart
+mv ./packages/$str/test/sample_test.dart ./packages/$str/test/$str\_test.dart
 melos bootstrap
 melos run pub:get
