9895403bda63818f2a5298ac88a512e02cb08991
DaigoWakabayashi <mamushi.journey@gmail.com>
2022-04-17 21:14:41 +0900

    #15 create gallery package

---
+++ packages/gallery/test/gallery_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gallery/gallery.dart';
+
+void main() {
+  test('Calculator クラスの addOne メソッドのテスト', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+  });
+
+  test('Calculator クラスの subtractOne メソッドのテスト', () {
+    final calculator = Calculator();
+    expect(calculator.subtractOne(2), 1);
+    expect(calculator.subtractOne(-7), -8);
+    expect(calculator.subtractOne(0), -1);
+  });
+}
