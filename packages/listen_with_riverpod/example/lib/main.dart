34e5f18cf0a3e9ddb1e3824f3b834e51b3de8e40
susa.keyber <susa@keyber.jp>
2022-06-13 06:26:07 +0900

    🚀 feat:  Firebase関連のコードを削除、その他コードの整理
    Issue #45

--- packages/listen_with_riverpod/example/lib/main.dart
+++ packages/listen_with_riverpod/example/lib/main.dart
@@ -1,15 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:listen_with_riverpod/listen_with_riverpod.dart';
 
-import 'firebase_options.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MyApp());
 }
 
@@ -28,84 +19,23 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
-
-  static const path = '/my-home/';
-  static const name = 'MyHome';
-
-  @override
-  MyHomePageState createState() => MyHomePageState();
-}
-
-class MyHomePageState extends State<MyHomePage> {
-  int _addingCounter = 0;
-  int _subtractingCounter = 10;
-  final DocumentReference<Map<String, dynamic>> _counterRef =
-      FirebaseFirestore.instance.collection('counter').doc('counter');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Sample'),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Adding Counter: $_addingCounter'),
-            const SizedBox(height: 8),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  _addingCounter = Calculator().addOne(_addingCounter);
-                });
-              },
-              child: const Text('Add one!'),
-            ),
-            const SizedBox(height: 16),
-            Text('Subtracting Counter: $_subtractingCounter'),
-            const SizedBox(height: 8),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  _subtractingCounter = Calculator().subtractOne(_subtractingCounter);
-                });
-              },
-              child: const Text('Subtract one!'),
-            ),
-            const SizedBox(height: 16),
-            StreamBuilder<int>(
-              stream: _firestoreCounter,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Text('Waiting...');
-                }
-                if (!snapshot.hasData) {
-                  return const Text('Adding Counter (Firestore): 0');
-                }
-                final counter = snapshot.data!;
-                return Text('Adding Counter (Firestore): $counter');
-              },
-            ),
-            const SizedBox(height: 8),
-            ElevatedButton(
-              onPressed: () async {
-                await _counterRef.set(
-                  <String, dynamic>{'counter': FieldValue.increment(1)},
-                  SetOptions(merge: true),
-                );
-              },
-              child: const Text('Add one!'),
-            ),
+          children: const [
+            Text('Sample'),
           ],
         ),
       ),
     );
-  }
-
-  Stream<int> get _firestoreCounter {
-    return _counterRef.snapshots().map((ds) {
-      return (ds.data() ?? <String, dynamic>{'counter': 0})['counter'] as int;
-    });
   }
 }
