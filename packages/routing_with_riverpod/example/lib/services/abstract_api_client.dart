8518e57051b8bf04acb01f803f94e35621c82aae
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-06-18 15:00:27 +0900

    feat: define api client classes

---
+++ packages/routing_with_riverpod/example/lib/services/abstract_api_client.dart
@@ -0,0 +1,56 @@
+import 'package:dio/dio.dart';
+
+import '../models/api/response_result/response_result.dart';
+
+/// dio.dart の abstract class Dio の形式に沿った
+/// API クライアントの抽象クラス
+abstract class AbstractApiClient {
+  Future<ResponseResult> get(
+    String path, {
+    Map<String, dynamic> queryParameters,
+    Map<String, dynamic> header,
+    Options options,
+    CancelToken cancelToken,
+    ProgressCallback onReceiveProgress,
+  });
+
+  Future<ResponseResult> put(
+    String path, {
+    Map<String, dynamic> data,
+    Map<String, dynamic> queryParameters,
+    Map<String, dynamic> header,
+    Options options,
+    CancelToken cancelToken,
+    ProgressCallback onSendProgress,
+    ProgressCallback onReceiveProgress,
+  });
+
+  Future<ResponseResult> patch(
+    String path, {
+    Map<String, dynamic> data,
+    Map<String, dynamic> queryParameters,
+    Map<String, dynamic> header,
+    CancelToken cancelToken,
+    ProgressCallback onReceiveProgress,
+  });
+
+  Future<ResponseResult> post(
+    String path, {
+    Map<String, dynamic> data,
+    Map<String, dynamic> queryParameters,
+    Map<String, dynamic> header,
+    Options options,
+    CancelToken cancelToken,
+    ProgressCallback onSendProgress,
+    ProgressCallback onReceiveProgress,
+  });
+
+  Future<ResponseResult> delete(
+    String path, {
+    Map<String, dynamic> data,
+    Map<String, dynamic> queryParameters,
+    Map<String, dynamic> header,
+    Options options,
+    CancelToken cancelToken,
+  });
+}
