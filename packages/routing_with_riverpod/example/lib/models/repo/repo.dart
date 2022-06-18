7cbbe90eb8bf782caf08bef8824c988937a2309c
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-06-18 14:57:48 +0900

    feat: define github repo model

---
+++ packages/routing_with_riverpod/example/lib/models/repo/repo.dart
@@ -0,0 +1,25 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../owner/owner.dart';
+
+part 'repo.freezed.dart';
+part 'repo.g.dart';
+
+@freezed
+class Repo with _$Repo {
+  const factory Repo({
+    @Default(0) int id,
+    @Default('') String name,
+    @Default(Owner.defaultValue) Owner owner,
+    @Default('') String htmlUrl,
+    @Default('') String description,
+    DateTime? updatedAt,
+    @Default(0) int starGazersCount,
+    @Default(0) int forksCount,
+    @Default(0) int watchersCount,
+  }) = _Repo;
+
+  factory Repo.fromJson(Map<String, dynamic> json) => _$RepoFromJson(json);
+
+  static const defaultValue = Repo();
+}
---
+++ packages/routing_with_riverpod/example/lib/models/repo/repo.freezed.dart
@@ -0,0 +1,334 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'repo.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Repo _$RepoFromJson(Map<String, dynamic> json) {
+  return _Repo.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Repo {
+  int get id => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+  Owner get owner => throw _privateConstructorUsedError;
+  String get htmlUrl => throw _privateConstructorUsedError;
+  String get description => throw _privateConstructorUsedError;
+  DateTime? get updatedAt => throw _privateConstructorUsedError;
+  int get starGazersCount => throw _privateConstructorUsedError;
+  int get forksCount => throw _privateConstructorUsedError;
+  int get watchersCount => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $RepoCopyWith<Repo> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $RepoCopyWith<$Res> {
+  factory $RepoCopyWith(Repo value, $Res Function(Repo) then) =
+      _$RepoCopyWithImpl<$Res>;
+  $Res call(
+      {int id,
+      String name,
+      Owner owner,
+      String htmlUrl,
+      String description,
+      DateTime? updatedAt,
+      int starGazersCount,
+      int forksCount,
+      int watchersCount});
+
+  $OwnerCopyWith<$Res> get owner;
+}
+
+/// @nodoc
+class _$RepoCopyWithImpl<$Res> implements $RepoCopyWith<$Res> {
+  _$RepoCopyWithImpl(this._value, this._then);
+
+  final Repo _value;
+  // ignore: unused_field
+  final $Res Function(Repo) _then;
+
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? name = freezed,
+    Object? owner = freezed,
+    Object? htmlUrl = freezed,
+    Object? description = freezed,
+    Object? updatedAt = freezed,
+    Object? starGazersCount = freezed,
+    Object? forksCount = freezed,
+    Object? watchersCount = freezed,
+  }) {
+    return _then(_value.copyWith(
+      id: id == freezed
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: name == freezed
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      owner: owner == freezed
+          ? _value.owner
+          : owner // ignore: cast_nullable_to_non_nullable
+              as Owner,
+      htmlUrl: htmlUrl == freezed
+          ? _value.htmlUrl
+          : htmlUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: description == freezed
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      updatedAt: updatedAt == freezed
+          ? _value.updatedAt
+          : updatedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      starGazersCount: starGazersCount == freezed
+          ? _value.starGazersCount
+          : starGazersCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      forksCount: forksCount == freezed
+          ? _value.forksCount
+          : forksCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      watchersCount: watchersCount == freezed
+          ? _value.watchersCount
+          : watchersCount // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+
+  @override
+  $OwnerCopyWith<$Res> get owner {
+    return $OwnerCopyWith<$Res>(_value.owner, (value) {
+      return _then(_value.copyWith(owner: value));
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$_RepoCopyWith<$Res> implements $RepoCopyWith<$Res> {
+  factory _$$_RepoCopyWith(_$_Repo value, $Res Function(_$_Repo) then) =
+      __$$_RepoCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int id,
+      String name,
+      Owner owner,
+      String htmlUrl,
+      String description,
+      DateTime? updatedAt,
+      int starGazersCount,
+      int forksCount,
+      int watchersCount});
+
+  @override
+  $OwnerCopyWith<$Res> get owner;
+}
+
+/// @nodoc
+class __$$_RepoCopyWithImpl<$Res> extends _$RepoCopyWithImpl<$Res>
+    implements _$$_RepoCopyWith<$Res> {
+  __$$_RepoCopyWithImpl(_$_Repo _value, $Res Function(_$_Repo) _then)
+      : super(_value, (v) => _then(v as _$_Repo));
+
+  @override
+  _$_Repo get _value => super._value as _$_Repo;
+
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? name = freezed,
+    Object? owner = freezed,
+    Object? htmlUrl = freezed,
+    Object? description = freezed,
+    Object? updatedAt = freezed,
+    Object? starGazersCount = freezed,
+    Object? forksCount = freezed,
+    Object? watchersCount = freezed,
+  }) {
+    return _then(_$_Repo(
+      id: id == freezed
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: name == freezed
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      owner: owner == freezed
+          ? _value.owner
+          : owner // ignore: cast_nullable_to_non_nullable
+              as Owner,
+      htmlUrl: htmlUrl == freezed
+          ? _value.htmlUrl
+          : htmlUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: description == freezed
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      updatedAt: updatedAt == freezed
+          ? _value.updatedAt
+          : updatedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      starGazersCount: starGazersCount == freezed
+          ? _value.starGazersCount
+          : starGazersCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      forksCount: forksCount == freezed
+          ? _value.forksCount
+          : forksCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      watchersCount: watchersCount == freezed
+          ? _value.watchersCount
+          : watchersCount // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_Repo implements _Repo {
+  const _$_Repo(
+      {this.id = 0,
+      this.name = '',
+      this.owner = Owner.defaultValue,
+      this.htmlUrl = '',
+      this.description = '',
+      this.updatedAt,
+      this.starGazersCount = 0,
+      this.forksCount = 0,
+      this.watchersCount = 0});
+
+  factory _$_Repo.fromJson(Map<String, dynamic> json) => _$$_RepoFromJson(json);
+
+  @override
+  @JsonKey()
+  final int id;
+  @override
+  @JsonKey()
+  final String name;
+  @override
+  @JsonKey()
+  final Owner owner;
+  @override
+  @JsonKey()
+  final String htmlUrl;
+  @override
+  @JsonKey()
+  final String description;
+  @override
+  final DateTime? updatedAt;
+  @override
+  @JsonKey()
+  final int starGazersCount;
+  @override
+  @JsonKey()
+  final int forksCount;
+  @override
+  @JsonKey()
+  final int watchersCount;
+
+  @override
+  String toString() {
+    return 'Repo(id: $id, name: $name, owner: $owner, htmlUrl: $htmlUrl, description: $description, updatedAt: $updatedAt, starGazersCount: $starGazersCount, forksCount: $forksCount, watchersCount: $watchersCount)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_Repo &&
+            const DeepCollectionEquality().equals(other.id, id) &&
+            const DeepCollectionEquality().equals(other.name, name) &&
+            const DeepCollectionEquality().equals(other.owner, owner) &&
+            const DeepCollectionEquality().equals(other.htmlUrl, htmlUrl) &&
+            const DeepCollectionEquality()
+                .equals(other.description, description) &&
+            const DeepCollectionEquality().equals(other.updatedAt, updatedAt) &&
+            const DeepCollectionEquality()
+                .equals(other.starGazersCount, starGazersCount) &&
+            const DeepCollectionEquality()
+                .equals(other.forksCount, forksCount) &&
+            const DeepCollectionEquality()
+                .equals(other.watchersCount, watchersCount));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(id),
+      const DeepCollectionEquality().hash(name),
+      const DeepCollectionEquality().hash(owner),
+      const DeepCollectionEquality().hash(htmlUrl),
+      const DeepCollectionEquality().hash(description),
+      const DeepCollectionEquality().hash(updatedAt),
+      const DeepCollectionEquality().hash(starGazersCount),
+      const DeepCollectionEquality().hash(forksCount),
+      const DeepCollectionEquality().hash(watchersCount));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_RepoCopyWith<_$_Repo> get copyWith =>
+      __$$_RepoCopyWithImpl<_$_Repo>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_RepoToJson(this);
+  }
+}
+
+abstract class _Repo implements Repo {
+  const factory _Repo(
+      {final int id,
+      final String name,
+      final Owner owner,
+      final String htmlUrl,
+      final String description,
+      final DateTime? updatedAt,
+      final int starGazersCount,
+      final int forksCount,
+      final int watchersCount}) = _$_Repo;
+
+  factory _Repo.fromJson(Map<String, dynamic> json) = _$_Repo.fromJson;
+
+  @override
+  int get id => throw _privateConstructorUsedError;
+  @override
+  String get name => throw _privateConstructorUsedError;
+  @override
+  Owner get owner => throw _privateConstructorUsedError;
+  @override
+  String get htmlUrl => throw _privateConstructorUsedError;
+  @override
+  String get description => throw _privateConstructorUsedError;
+  @override
+  DateTime? get updatedAt => throw _privateConstructorUsedError;
+  @override
+  int get starGazersCount => throw _privateConstructorUsedError;
+  @override
+  int get forksCount => throw _privateConstructorUsedError;
+  @override
+  int get watchersCount => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$$_RepoCopyWith<_$_Repo> get copyWith => throw _privateConstructorUsedError;
+}
