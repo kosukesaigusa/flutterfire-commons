200eb65ddc48be498769fe058a9086ed3ef88177
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-06-18 14:59:46 +0900

    feat: add json_converter

---
+++ packages/routing_with_riverpod/example/lib/models/json_converter.dart
@@ -0,0 +1,24 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../constants/map.dart';
+
+///
+class BaseResponseDataConverter implements JsonConverter<Map<String, dynamic>, dynamic> {
+  const BaseResponseDataConverter();
+
+  @override
+  Map<String, dynamic> fromJson(dynamic data) {
+    if (data == null) {
+      return emptyMap;
+    }
+    if (data is List) {
+      // データがリストである場合は
+      // 'items' のキーをつけて Map<String, List<dynamic>> にする。
+      return <String, List<dynamic>>{'items': data};
+    }
+    return data as Map<String, dynamic>;
+  }
+
+  @override
+  Map<String, dynamic> toJson(Map<String, dynamic> data) => data;
+}
