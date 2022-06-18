e8d6f78dde7774966e8f085bb00f0181ae93ec05
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-06-18 14:56:16 +0900

    feat: define constant values

---
+++ packages/routing_with_riverpod/example/lib/constants/string.dart
@@ -0,0 +1,7 @@
+// "Referrer" の誤字 (https://en.wikipedia.org/wiki/HTTP_referer) でややこしいので定数化しておく
+const referrer = 'Referer';
+
+// エラーメッセージ関係で複数回使用しているもの
+const generalExceptionMessage = 'エラーが発生しました。';
+const networkNotConnected = 'ネットワーク接続がありません。';
+const responseFormatNotValid = 'レスポンスの形式が正しくありません。';
